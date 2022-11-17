23aeae52a44b9689ce33c6a80083b85926fdc3fc
kobayashimasanobu <m.kobayashi@4nonome.com>
2022-11-17 17:25:09 +0900

    レビューよろしくお願いします

---
+++ colab/answer/pandas_answer/pandas_10.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df_customer = pd.read_csv('https://raw.githubusercontent.com/The-Japan-DataScientist-Society/100knocks-preprocess/master/docker/work/data/customer.csv')
+df_product = pd.read_csv('https://raw.githubusercontent.com/The-Japan-DataScientist-Society/100knocks-preprocess/master/docker/work/data/product.csv')
+df_receipt = pd.read_csv('https://raw.githubusercontent.com/The-Japan-DataScientist-Society/100knocks-preprocess/master/docker/work/data/receipt.csv')
+
+df_product_1 = df_product.dropna()
+len(df_product), len(df_product_1)
---
+++ colab/answer/pandas_answer/pandas_4.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+df_customer = pd.read_csv('https://raw.githubusercontent.com/The-Japan-DataScientist-Society/100knocks-preprocess/master/docker/work/data/customer.csv')
+df_product = pd.read_csv('https://raw.githubusercontent.com/The-Japan-DataScientist-Society/100knocks-preprocess/master/docker/work/data/product.csv')
+df_receipt = pd.read_csv('https://raw.githubusercontent.com/The-Japan-DataScientist-Society/100knocks-preprocess/master/docker/work/data/receipt.csv')
+
+print(df_receipt['customer_id'].nunique())
